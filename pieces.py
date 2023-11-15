f64593537ccde2908f1a94c88709cb9e8833738b
hazzawinchester <137170400+hazzawinchester@users.noreply.github.com>
2023-11-15 08:55:17 +0000

    piece class now works with images

--- pieces.py
+++ pieces.py
@@ -1,7 +1,30 @@
 import tkinter as tk
+from PIL import ImageTk, Image
+
+pieces_revesed = {'♟': 'p', '♞': 'n', '♝': 'b', '♜': 'r', '♛': 'q', '♚': 'k', '♙': 'P', '♘': 'N', '♗': 'B', '♖': 'R', '♕': 'Q', '♔': 'K'}
+file_type = {"classic":"png","periodic":"png"}
+
+#
+#https://thenounproject.com FOR ICONS 
+#
 
 class Piece(tk.Label):
-    def __init__(self,master,pos,bg=None,text=None,font=None,borderwidth=None):
-        super().__init__(master,text=text,bg=bg,font=font,borderwidth=borderwidth)
+    def __init__(self,master,piece,row,col,piece_type):
+        if piece != '':
+            if pieces_revesed[piece].isupper():
+                self.img = Image.open(f"Pieces/{piece_type}/{piece_type}w{pieces_revesed[piece].lower()}.{file_type[piece_type]}")
+            else: 
+                self.img = Image.open(f"Pieces/{piece_type}/{piece_type}b{pieces_revesed[piece]}.{file_type[piece_type]}")
+                
+            self.img = self.img.resize((90, 90))
+            self.img = ImageTk.PhotoImage(self.img)
+            super().__init__(master,borderwidth=0,image=self.img,bg="pink")
+
+            self.colour = "w" if pieces_revesed[piece].isupper() else "b"
+        else:
+            super().__init__(master,borderwidth=0,bg="white" if (row+col)%2==0 else "gray")
+
+            self.colour = None
+
         self.master=master
-        self.position = pos
+ 
