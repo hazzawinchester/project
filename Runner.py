f53ce20666056673de7c1e777335148648b92267
hazzawinchester <137170400+hazzawinchester@users.noreply.github.com>
2023-11-15 08:55:48 +0000

    create board function so it can be recalled to restart game

--- Runner.py
+++ Runner.py
@@ -4,13 +4,17 @@ import numpy as np
 import board
 
 
+def create_board(layout="rnbqkbnr/pppppppp/8/8/p7/8/PPPPPPPP/RNBQKBNR",piece_type="classic"):
+    chessboard = board.chessboard(window,layout,piece_type)
+    chessboard.place(relx=0.5,rely=0.5, anchor="center")
+
 window = tk.Tk()
 window.title("Chess")
 window.geometry("800x800")
+#window.wm_attributes("-transparentcolor","black")
 
-chessboard = board.chessboard(window,"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR")
-chessboard.place(relx=0.5,rely=0.5, anchor="center",)
-window.wm_attributes('-transparentcolor','black')
+
+create_board(piece_type="periodic")
+
 
 window.mainloop()
-
